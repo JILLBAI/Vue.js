f7f1dde5ea6dbeba2df24e99e091775a68640aef
JILLBAI <tiger90483@gmail.com>
2020-07-19 12:23:33 +0800

    R

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 console.log("我是js")
 // 定義渲染區域 變數vue1= new vue ,el:"" 指定元素
-var vue1=new vue1({
-    el:"#vue1"
+var vue1=new Vue({
+    el: "#vue1",
+    data:{
+        text:"哈囉<我是Vue!",
+        aligment:"center",
+        yahoo:"https://tw.yahoo.com/"
+    }
 });
